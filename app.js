6b7cc8ae40c5e64103ce322a32e7fcd93af5955a
ishuoncode <ishusingh8127@gmail.com>
2023-03-23 01:09:06 +0530

    added heroku config

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
+
+app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('veiw', path.join(__dirname, 'views'));
 
